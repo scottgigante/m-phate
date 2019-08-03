9668d3a94190811fe6ffb96370528c279236b9ed
Scott Gigante <scottgigante@gmail.com>
2019-08-03 14:16:55 -0400

    allow extra arguments to generalization training

--- scripts/generalization/generalization_train.sh
+++ scripts/generalization/generalization_train.sh
@@ -4,10 +4,12 @@ else
     DATA_DIR=$1
 fi
 
+EXTRA_ARGS=$2
+
 mkdir -p ${DATA_DIR}/generalization
-python scripts/generalization/classifier_mnist_generalization_train.py --save-dir ${DATA_DIR} &
-python scripts/generalization/classifier_mnist_generalization_train.py --dropout 0.5 --save-dir ${DATA_DIR} &
-python scripts/generalization/classifier_mnist_generalization_train.py --scrambled --save-dir ${DATA_DIR} &
+python scripts/generalization/classifier_mnist_generalization_train.py ${EXTRA_ARGS} --save-dir ${DATA_DIR} &
+python scripts/generalization/classifier_mnist_generalization_train.py ${EXTRA_ARGS} --dropout 0.5 --save-dir ${DATA_DIR} &
+python scripts/generalization/classifier_mnist_generalization_train.py ${EXTRA_ARGS} --scrambled --save-dir ${DATA_DIR} &
 for regularizer in l1 l2; do
   for regularize in kernel activity; do
     python scripts/generalization/classifier_mnist_generalization_train.py --regularize $regularize --regularizer $regularizer --save-dir ${DATA_DIR} &
